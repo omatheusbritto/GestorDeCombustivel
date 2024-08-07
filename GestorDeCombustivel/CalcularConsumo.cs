f65bee778281c462e3168f758e66a0b5995c2fd3
MATHEUS BRITTO <202407139019@alunos.estacio.br>
2024-08-07 10:44:58 -0300

    Adicionar arquivos de projeto.

---
+++ GestorDeCombustivel/CalcularConsumo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calcular
+{
+    class CalcularConsumo
+    {
+        public double Litros, Km, ValorAbastecido;
+        public int TipoDeCombustivel;
+
+        public double ValorPorLitro()
+        {
+            double valorlitro = ValorAbastecido / Litros;
+            return valorlitro;
+        }
+        public double ValorPorKm()
+        {
+            double valorkm = ValorAbastecido / Km;
+            return valorkm;
+        }
+        public double KmPorLitro()
+        {
+            double kmlitro = Km / Litros;
+            return kmlitro;
+        }
+        public override string ToString()
+        {
+            return
+                $"Valor Abastecido foi R${ValorAbastecido:f2} | Qtda de Litros {Litros:f2} | Km Rodado {Km:f1}\n"
+                + $"Valor por Litros R${ValorPorLitro():f2} | Valor por Km R${ValorPorKm():f2} | Km por Litro {KmPorLitro():f1}";
+        }
+    }
+}
+
---
+++ GestorDeCombustivel/Program.cs
@@ -0,0 +1,29 @@
+﻿// Programa que calcula comsumo de combustivel, mostrando km/litros e R$/litros
+using System;
+using System.Globalization;
+
+namespace GestorDeConsumoDeCombustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calcular.CalcularConsumo usuario = new Calcular.CalcularConsumo();
+
+            Console.WriteLine("Programa de gestão de combustivel");
+            Console.WriteLine("\n");
+            Console.WriteLine("Tipo de combustivel abastecido \n[1] Etanol | [2] Gasolina");
+            usuario.TipoDeCombustivel = int.Parse(Console.ReadLine());
+            Console.Write("Quantos litros foi abastecido: ");
+            usuario.Litros = double.Parse(Console.ReadLine());
+            Console.Write("Valor do abastecimento: R$");
+            usuario.ValorAbastecido = double.Parse(Console.ReadLine());
+            Console.Write("Km rodados: ");
+            usuario.Km = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            Console.WriteLine(usuario);
+        }
+    }
+}
